b7f2ae53ff096cbf123e9cdeac92a6c56972d393
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-23 13:09:54 -0300

    smaller

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,10 @@ import Logo from "~/assets/icons/logo.svg"
 export function Header() {
   return (
     <header className="mx-auto flex h-24 items-center justify-center bg-secondary-600">
-      <span className="mr-4 h-16">
+      <span className="mr-4 h-8">
         <Logo />
       </span>
-      <span className="text-2xl font-bold text-secondary-100">Ignite Feed</span>
+      <span className="text-xl font-bold text-secondary-100">Ignite Feed</span>
     </header>
   )
 }
