e02029febb882779a7f32c6396fc3bbe3a7e23ad
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-23 13:08:43 -0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,12 @@
+import Logo from "~/assets/icons/logo.svg"
+
 export function Header() {
-  return <header className="h-24 bg-secondary-600">sou um header</header>
+  return (
+    <header className="mx-auto flex h-24 items-center justify-center bg-secondary-600">
+      <span className="mr-4 h-16">
+        <Logo />
+      </span>
+      <span className="text-2xl font-bold text-secondary-100">Ignite Feed</span>
+    </header>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
+
+import svgr from "vite-plugin-svgr"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true
+    })
+  ],
   server: {
     port: 3000
   },
