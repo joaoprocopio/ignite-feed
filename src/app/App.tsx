5a57ea703199416171011a4934c9c18e6e93a8ff
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-23 13:24:51 -0300

    start sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,28 @@
-import { Header } from "~/components"
+import { Header, Post, Sidebar } from "~/components"
 
 export function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <div>
+        <Sidebar />
+        <main>
+          <Post
+            post={{
+              author: "João Procópio",
+              content:
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, impedit harum? Odio maiores eaque labore laboriosam neque sit veritatis quo accusantium, rem ea in nulla, quae earum, illo rerum ab!"
+            }}
+          />
+          <Post
+            post={{
+              author: "Willian Bittencourt",
+              content:
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, impedit harum? Odio maiores eaque labore laboriosam neque sit veritatis quo accusantium, rem ea in nulla, quae earum, illo rerum ab!"
+            }}
+          />
+        </main>
+      </div>
+    </>
+  )
 }
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,3 +1,17 @@
-export function Post({ author }: { author: string }) {
-  return <div>{author}</div>
+interface Post {
+  author: string
+  content: string
+}
+
+interface Props {
+  post: Post
+}
+
+export function Post({ post }: Props) {
+  return (
+    <>
+      <div>{post.author}</div>
+      <div>{post.content}</div>
+    </>
+  )
 }
