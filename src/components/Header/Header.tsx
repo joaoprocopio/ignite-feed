d0d6d02b4e3a86e85aed439515ee1ae518b8533a
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-23 12:46:23 -0300

    tailwind configurado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,3 @@
-import classes from "./Header.module.css"
-
 export function Header() {
-  return <header className={classes.header}>sou um header</header>
+  return <header>sou um header</header>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+import { fontFamily } from "tailwindcss/defaultTheme"
+
 /** @type {import("tailwindcss").Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx,css}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          500: "#00875F",
+          400: "#00B37E"
+        },
+        secondary: {
+          700: "#121214",
+          600: "#202024",
+          500: "#323238",
+          400: "#7C7C8A",
+          300: "#8D8D99",
+          200: "#C4C4CC",
+          100: "#E1E1E6"
+        },
+        danger: "#F75A68"
+      },
+      fontFamily: {
+        sans: ["Roboto", ...fontFamily.sans]
+      }
+    }
   },
   plugins: []
 }
