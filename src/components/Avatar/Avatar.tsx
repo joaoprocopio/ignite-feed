ef8960fb9f3b3df2e009be4aab1c2cdd71c7f23d
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-07-24 18:50:35 -0300

    css novo

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,8 +2,6 @@ import classNames from "classnames"
 
 import styles from "./Avatar.module.scss"
 
-console.log(styles)
-
 export function Avatar({ src, outlined, customClass }: { src: string; outlined?: boolean; customClass?: string }) {
   const classes = classNames({
     [styles.avatar]: true,
